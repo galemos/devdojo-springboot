d9803dca4b7eaa686d7bd4e9d05f36863d29bddb
glemos <sollua135>
2018-06-18 13:33:33 -0300

    Aula 11 Devdojo - Aplicação não está subindo, refazer os passos desta aula

---
+++ src/main/java/br/com/devdojo/awsome/handler/RestExceptionHandler.java
@@ -0,0 +1,29 @@
+package br.com.devdojo.awsome.handler;
+
+import br.com.devdojo.awsome.error.ResourceNotFoundDetails;
+import br.com.devdojo.awsome.error.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+/**
+ * @author Created by glemos on 18/06/2018.
+ */
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfException){
+        ResourceNotFoundDetails rnfDetails = ResourceNotFoundDetails.Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.NOT_FOUND.value())
+                .title("Resource not found")
+                .detail(rnfException.getMessage())
+                .developerMessage(rnfException.getClass().getName())
+                .build();
+        return new ResponseEntity<>(rnfDetails, HttpStatus.NOT_FOUND);
+    }
+}
