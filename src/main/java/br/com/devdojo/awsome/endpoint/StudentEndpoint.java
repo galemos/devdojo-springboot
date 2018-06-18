390c9cdcd1e80a20ca2fcab21700a282136066bb
glemos <sollua135>
2018-06-18 10:23:30 -0300

    Aula 6 Devdojo

--- src/main/java/br/com/devdojo/awsome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awsome/endpoint/StudentEndpoint.java
@@ -10,14 +10,7 @@ import br.com.devdojo.awsome.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("students")
@@ -46,6 +39,12 @@ public class StudentEndpoint {
         } else {
             return  new ResponseEntity<>(Student.studentList.get(index),HttpStatus.OK);
         }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student,HttpStatus.OK);
     }
 
 
--- src/test/java/br/com/devdojo/awsome/ApplicationStartTest.java
+++ src/test/java/br/com/devdojo/awsome/ApplicationStartTest.java
@@ -1,5 +1,9 @@
 package br.com.devdojo.awsome;
 
+/**
+ * @author Created by Gabriel Lemos on 13/06/2018.
+ */
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
